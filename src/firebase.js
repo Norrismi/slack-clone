cf76dd37c3f66de076361b27c9615a74d882c002
Norrismi <Norrismi0@gmail.com>
2021-07-29 16:45:53 -0400

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCkxFgpH2WkGV3DMLVoyBd4TfR0Q_AmoyM",
+    authDomain: "slack-redux-yt.firebaseapp.com",
+    projectId: "slack-redux-yt",
+    storageBucket: "slack-redux-yt.appspot.com",
+    messagingSenderId: "430859242449",
+    appId: "1:430859242449:web:8f10663c8aa5e059eeaef0"
+  };
